c08779b02e898e57961f4ef4b640763b93ac3287
Turan Sadigova <sadigova.turan@gmail.com>
2022-09-28 16:25:46 +0400

    Update usage example

--- nasa_program.rb
+++ nasa_program.rb
@@ -1,11 +1,10 @@
-# frozen_string_literal: true
+# Usage example:
+# FuelCalculator.calculate(75432,
+#                         [[:launch, 9.807], [:land, 1.62], [:launch, 1.62],
+#                         [:land, 3.711], [:launch, 3.711], [:land, 9.807]])
+# => 212161
 
 module FuelCalculator
-  # Example:
-  # FuelCalculator.calculate(28801,
-  #                          [[:launch, 9.807], [:land, 1.62],
-  #                           [:launch, 1.62],  [:land, 9.807]])
-
   class << self
     def calculate(mass, mission_data)
       mission_data.reverse.reduce(0) do |sum, route|
